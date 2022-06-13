838ffa95269f2ddd4c0f1ce7cc9aa0e36fa881bf
Berk Basdemir <basdemirberk@gmail.com>
2022-06-13 23:40:04 +0300

    MealConfiguration ve MealDetailConfiguration Yüklendi.

---
+++ DietBoost.DAL/Configurations/MealDetailCofiguration.cs
@@ -0,0 +1,20 @@
+﻿using ProjeTaslak.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DietBoost.DAL.Configurations
+{
+    public class MealDetailCofiguration : EntityTypeConfiguration<MealDetail>
+    {
+        public MealDetailCofiguration()
+        {
+            HasKey(a => new { a.MealID, a.FoodID });
+            HasRequired(a=> a.Meal).WithMany(b=>b.MealDetails).HasForeignKey(c => c.MealID);
+            HasRequired(a=> a.Food).WithMany(b=>b.MealDetails).HasForeignKey(c => c.FoodID);
+        }
+    }
+}
