68d370b2694435fe84d7fcb5229ccf27d5be351e
Sinem93 <snmpmk93@gmail.com>
2022-06-14 22:33:51 +0300

    Food configuration eklendi

---
+++ DietBoost.DAL/Configurations/FoodConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DietBoost.DAL.Configurations
+{
+    public class FoodConfiguration : EntityTypeConfiguration<Food>
+    {
+        public FoodConfiguration()
+        {
+            HasKey(a => a.FoodId);
+            Property(a => a.Name).IsRequired().HasMaxLength(50);
+
+            HasRequired(a => a.Categories).WithMany(a => a.Foods).HasForeignKey(a => a.CategoryID);
+        }
+    }
+
+}
